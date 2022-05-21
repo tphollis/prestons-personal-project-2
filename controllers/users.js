8be9af000b2d379926da2c8394b30147b1b0af9d
tphollis <thomas.preston.hollis@outlook.com>
2022-05-21 15:19:37 -0600

    first commit

---
+++ controllers/users.js
@@ -0,0 +1,69 @@
+const mongodb = require('../db/connect');
+const ObjectId = require('mongodb').ObjectId;
+
+const getAll = async (req, res, next) => {
+  const result = await mongodb.getDb().db().collection('users').find();
+  result.toArray().then((lists) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(lists);
+  });
+};
+
+const getSingle = async (req, res, next) => {
+  const userId = new ObjectId(req.params.id);
+  const result = await mongodb.getDb().db().collection('users').find({ _id: userId });
+  result.toArray().then((lists) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(lists[0]);
+  });
+};
+
+const createUser = async (req, res) => {
+  const user = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    password: req.body.password,
+    birthday: req.body.birthday,
+    privelage: req.body.privelage
+  };
+  const response = await mongodb.getDb().db().collection('users').insertOne(user);
+  if (response.acknowledged) {
+    res.status(201).json(response);
+  } else {
+    res.status(500).json(response.error || 'There was an error when creating the user.');
+  }
+};
+
+const updateUser = async (req, res) => {
+  const userId = new ObjectId(req.params.id);
+  // be aware of updateOne if you only want to update specific fields
+  const user = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    password: req.body.password,
+    birthday: req.body.birthday,
+    privelage: req.body.privelage
+  };
+  const response = await mongodb.getDb().db().collection('users').replaceOne({ _id: userId }, user);
+  console.log(response);
+  if (response.modifiedCount > 0) {
+    res.status(204).send();
+  } else {
+    res.status(500).json(response.error || 'There was an error when updating the user.');
+  }
+};
+
+const deleteUser = async (req, res) => {
+  const userId = new ObjectId(req.params.id);
+  const response = await mongodb.getDb().db().collection('users').remove({ _id: userId }, true);
+  console.log(response);
+  if (response.deletedCount > 0) {
+    res.status(204).send();
+  } else {
+    res.status(500).json(response.error || 'There was an error when deleting the user.');
+  }
+};
+
+module.exports = { getAll, getSingle, createUser, updateUser, deleteUser};
